016f33a14c5b9420e2df37d92a4b3f402f7b8b3c
rudyalvaradobeltran <rudy.alvarado.b@gmail.com>
2025-04-09 22:47:21 -0400

    Initial commit

---
+++ test/aws-cdk-email-my-bitcoin.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkEmailMyBitcoin from '../lib/aws-cdk-email-my-bitcoin-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-email-my-bitcoin-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkEmailMyBitcoin.AwsCdkEmailMyBitcoinStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
